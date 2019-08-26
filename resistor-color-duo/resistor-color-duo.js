15978de44220ebe76f21598088f27000497d5fba
n0001150 <Jotham.Hoag@LibertyMutual.com>
2019-08-26 15:51:04 -0400

    Add several exercises

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,7 @@
+
+export const value = () => {
+  return COLORS.indexOf("brown") + COLORS.indexOf("black");
+};
+
+const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey", "white"];
+
